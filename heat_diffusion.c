4cab74d9a32ad1db1fc9c0a4a28ff7dac025a5fb
Jens Nilsson <jenszza@gmail.com>
2018-10-18 11:05:42 +0200

    OpenCL setup

--- heat_diffusion.c
+++ heat_diffusion.c
@@ -1,3 +1,4 @@
+#include <CL/cl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -68,5 +69,46 @@ int main(int argc, char *argv[]){
 		&n_iterations
 	);
 	
+
+
+	cl_int error;
+	//get platform info
+  	cl_platform_id platform_id;
+	cl_uint nmb_platforms;
+  	if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
+    		printf( "cannot get platform\n" );
+    		return 1;
+  	}
+	//get device info
+	cl_device_id device_id;
+  	cl_uint nmb_devices;
+  	if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1,
+                &device_id, &nmb_devices) != CL_SUCCESS) {
+    		printf( "cannot get device\n" );
+    		return 1;
+  	}
+	//setup context
+	cl_context context;
+  	cl_context_properties properties[] =
+  	{
+    		CL_CONTEXT_PLATFORM,
+    		(cl_context_properties) platform_id,
+    		0
+ 	};
+  	context = clCreateContext(properties, 1, &device_id, NULL, NULL, &error);
+
+	//setup command queue
+	cl_command_queue command_queue;
+  	command_queue = clCreateCommandQueue(context, device_id, 0, &error);
+  	if (error != CL_SUCCESS) {
+    		printf("cannot create context\n");
+    		return 1;
+  	}
+
+
+
+	//release context and command queue
+	clReleaseCommandQueue(command_queue);
+	clReleaseContext(context);
 	return 0;
 }
