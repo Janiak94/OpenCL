48b1f03a220efedad493ba70f39b03dedb74a3fe
Jens Nilsson <jenszza@gmail.com>
2018-10-18 23:07:14 +0200

    Calculating averages, reduction using local memory

--- cl_heat_diffusion.cl
+++ cl_heat_diffusion.cl
@@ -1,12 +1,13 @@
 __kernel void
 local_heat_diffusion(
-	__global const long * h,
+	__global const double * h,
 	const float c,
 	const unsigned int width,
 	const unsigned int height,
-	__global long * h_updated
+	__global double * h_updated
 )
 {
+	//thought: handle with global barrier, will only need array that is both input and output
 	unsigned int array_idx = get_global_id(0);
 	unsigned int ix = array_idx/height;
 	unsigned int jx = array_idx%width;
@@ -33,5 +34,41 @@ local_heat_diffusion(
 	}
 	h_updated[height*ix+jx] = h[height*ix+jx] +
 		 c*(local_temp/4-h[height*ix+jx]);
+
 }
+
+__kernel void
+move_buffer(
+	__global double * m1,
+	__global const double * m2
+)
+{
+	unsigned int array_idx = get_global_id(0);
+	m1[array_idx] = m2[array_idx];
+}	
+
+__kernel void
+partial_sum(
+	__global const double * h,
+	__local double * local_sum,
+	__global double * partial_sum
+)
+{
+	uint local_id = get_local_id(0);
+	uint group_size = get_local_size(0);
+
+	//get the local element we wish to add to the average
+	local_sum[local_id] = h[get_global_id(0)];
+
+	//will only work if the number of elements in h is even
+	for(uint i = group_size/2; i > 0; i/=2){
+		barrier(CLK_LOCAL_MEM_FENCE);
+		if(local_id < i){
+			local_sum[local_id] += local_sum[local_id + i];
+		}
+	}
 	
+	if(local_id == 0){
+		partial_sum[get_group_id(0)] = local_sum[0];
+	}
+}
--- heat_diffusion.c
+++ heat_diffusion.c
@@ -10,7 +10,7 @@ void set_args(
 	char *argv[],
 	size_t *height,
 	size_t *width,
-	long *initial_central,
+	double *initial_central,
 	float *diffusion_constant,
 	size_t *n_iterations
 ){
@@ -22,11 +22,11 @@ void set_args(
 		if(argv[i][0] == '-' && argv[i][1] == 'i'){
 			char *ptr;
 			short e;
-			*initial_central = strtol(&argv[i][2],&ptr,10);
+			*initial_central = (double)strtol(&argv[i][2],&ptr,10);
 			if(*ptr == 'e'){
 				e = (short) strtol(ptr+1,NULL,10);
 				for(unsigned short i = 0; i < e; ++i){
-					*initial_central*=10;
+					*initial_central*=10.0;
 				}
 			}else if(*ptr != '\0'){
 				printf("Wrong format: %s\n", ptr);
@@ -54,32 +54,39 @@ void set_args(
 	}
 }
 
-void initialize_grid(long **input,long **output, size_t height, size_t width,
-	long initial){
-	*output = calloc(height*width, sizeof(long));
-	*input = calloc(height*width, sizeof(long));
-
+//set the initial center value and allocate input and output arrays
+void initialize_grid(
+	double **input,
+	double **output,
+	size_t height,
+	size_t width,
+	double initial
+){
+	*output = calloc(height*width, sizeof(double));
+	*input = calloc(height*width, sizeof(double));
+	//this is buggy, fix
 	if(height%2 && width%2){
-		input[0][height*height/2+width/2-1] = initial;
+		input[0][(height-1)*height/2+width/2] = initial;
 	}else if(height%2 == 0 && width%2 == 0){
-		input[0][height*height/2+width/2] = initial/4;
+		input[0][height*(height/2)+width/2] = initial/4;
 		input[0][height*(height/2-1)+width/2] = initial/4;
 		input[0][height*height/2+width/2-1] = initial/4;
 		input[0][height*(height/2-1)+width/2-1] = initial/4;
 	}else if(height%2 == 0){
-		input[height*height/2+width/2] = initial/4;
+		input[0][height*height/2+width/2] = initial/4;
 		input[0][height*(height/2-1)+width/2] = initial/4;
 	}else{
-		input[height*height/2+width/2] = initial/4;
+		input[0][(height-1)*height/2+width/2-1] = initial/4;
 		input[0][height*height/2+width/2-1] = initial/4;
 	}
 	//print result here
 	for(int i = 0; i < height; ++i){
 		for(int j = 0; j < width; ++j){
-			printf("%ld ", input[0][height*i+j]);
+	//		printf("%.2f ", input[0][height*i+j]);
 		}
-		printf("\n");
+	//	printf("\n");
 	}
+	//	printf("\n");
 }
 
 int main(int argc, char *argv[]){
@@ -87,7 +94,7 @@ int main(int argc, char *argv[]){
  	 * read and set arguments
  	 */
 	size_t height, width, n_iterations;
-	long initial_central;
+	double initial_central;
 	float diffusion_constant;	
 	set_args(
 		argc,
@@ -173,34 +180,76 @@ int main(int argc, char *argv[]){
 	}
 	free(source_str);
 	
-	//create kernel
-	cl_kernel kernel;
-	kernel = clCreateKernel(program, "local_heat_diffusion", &error);
+	//create kernel for heat diffusion
+	cl_kernel kernel_heat;
+	kernel_heat = clCreateKernel(program, "local_heat_diffusion", &error);
 	if(error != CL_SUCCESS){
-		printf("Error while creating kernel\n");
+		printf("Error while creating heat kernel\n");
 	}
-	
-	//create buffers
+	//create kernel for moving output to input between iterations
+	cl_kernel kernel_move;
+	kernel_move = clCreateKernel(program, "move_buffer", &error);
+	if(error != CL_SUCCESS){
+		printf("Error while creating move kernel\n");
+	}
+	//create kernel for calculation of partial sum of temperatures
+	cl_kernel kernel_partial;
+	kernel_partial = clCreateKernel(program, "partial_sum", &error);
+	if(error != CL_SUCCESS){
+		printf("Error while creating partial kernel\n");
+	}
+	//create kernel for calculation of average diff
+/*	cl_kernel kernel_diff;
+	kernel_diff = clCreateKernel(program, "average_difference", &error);
+	if(error != CL_SUCCESS){
+		printf("Error while creating diff kernel\n");
+	}
+*/
+	//create buffers to hold grid before and after 
 	cl_mem input_buffer, output_buffer;
 	input_buffer = clCreateBuffer(
-		context, CL_MEM_READ_ONLY, sizeof(long)*ix_m, NULL, &error);
+		context, CL_MEM_READ_WRITE, sizeof(double)*ix_m, NULL, &error);
 	if(error != CL_SUCCESS){
 		printf("Error while creating input buffer\n");
 		return 1;
 	}
 	output_buffer = clCreateBuffer(
-		context, CL_MEM_WRITE_ONLY, sizeof(long)*ix_m, NULL, &error);
+		context, CL_MEM_WRITE_ONLY, sizeof(double)*ix_m, NULL, &error);
 	if(error != CL_SUCCESS){
 		printf("Error while creating output buffer\n");
 		return 1;
 	}
+	//probe the max work group size for the partial sums, so a
+	//partial sums buffer can be created
+	size_t max_work_group_size;
+	if(clGetKernelWorkGroupInfo(kernel_partial, device_id, CL_KERNEL_WORK_GROUP_SIZE,
+                         sizeof(size_t), &max_work_group_size, NULL) != CL_SUCCESS){
+		printf("Error when fetching max work group size\n");
+		return 1;
+	}
 
-	long *input, *output;
+	// create local sum buffer	
+	cl_mem local_sums_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(double)*max_work_group_size,NULL,&error);
+	if(error != CL_SUCCESS){
+		printf("Error creating local buffer\n");
+		return 1;
+	}
+	size_t n_work_groups = (ix_m/max_work_group_size == 0 ? 1024/32 : ix_m/max_work_group_size);
+	cl_mem partial_sums_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+		sizeof(double)*n_work_groups,NULL,&error);
+	if(error != CL_SUCCESS){
+		printf("Error creating partial sums buffer\n");
+		return 1;
+	}
+	
+	//create input and output arrays
+	double *input, *output;
 	initialize_grid(&input,&output, height, width, initial_central);
 	
-	//enqueue buffers
+	
+	//enqueue input buffer
 	if(clEnqueueWriteBuffer(command_queue, input_buffer, CL_TRUE,
-  		0, ix_m*sizeof(long), input, 0, NULL, NULL) != CL_SUCCESS){
+  		0, ix_m*sizeof(double), input, 0, NULL, NULL) != CL_SUCCESS){
 		printf("Error enqueueing input\n");
 		free(input);
 		free(output);
@@ -208,31 +257,88 @@ int main(int argc, char *argv[]){
 	}
 	
 	//set kernel arguments
-	if(clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer) != CL_SUCCESS ||
-	clSetKernelArg(kernel, 1, sizeof(float), &diffusion_constant) != CL_SUCCESS ||
-	clSetKernelArg(kernel, 2, sizeof(unsigned int), &width) != CL_SUCCESS ||
-	clSetKernelArg(kernel, 3, sizeof(unsigned int), &height) != CL_SUCCESS ||
-	clSetKernelArg(kernel, 4, sizeof(cl_mem), &output_buffer) != CL_SUCCESS){
-		printf("Error passing argumnets to kernal\n");
+	if(clSetKernelArg(kernel_heat, 0, sizeof(cl_mem), &input_buffer) != CL_SUCCESS ||
+	clSetKernelArg(kernel_heat, 1, sizeof(float), &diffusion_constant) != CL_SUCCESS ||
+	clSetKernelArg(kernel_heat, 2, sizeof(unsigned int), &width) != CL_SUCCESS ||
+	clSetKernelArg(kernel_heat, 3, sizeof(unsigned int), &height) != CL_SUCCESS ||
+	clSetKernelArg(kernel_heat, 4, sizeof(cl_mem), &output_buffer) != CL_SUCCESS){
+		printf("Error passing argumnets to heat kernal\n");
 		free(input);
 		free(output);
 		return 1;
 	}
-	
+	if(clSetKernelArg(kernel_move, 0, sizeof(cl_mem), &input_buffer) != CL_SUCCESS ||
+	clSetKernelArg(kernel_move, 1, sizeof(cl_mem), &output_buffer) != CL_SUCCESS){
+		printf("Error passing argumnets to move kernal\n");
+		free(input);
+		free(output);
+		return 1;
+	}
+	if(clSetKernelArg(kernel_partial, 0, sizeof(cl_mem), &output_buffer) != CL_SUCCESS ||
+	clSetKernelArg(kernel_partial, 1, sizeof(cl_mem), NULL) != CL_SUCCESS ||
+	clSetKernelArg(kernel_partial, 2, sizeof(cl_mem), &partial_sums_buffer) != CL_SUCCESS){
+		printf("Error passing argumnets to partial sums kernal\n");
+		free(input);
+		free(output);
+		return 1;
+	}
+		
 	//set length of argumnet vector
 	const size_t global = ix_m;
 	//Execute
-	if(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
+	if(clEnqueueNDRangeKernel(command_queue, kernel_heat, 1, NULL,
   		(const size_t *)&global, NULL, 0, NULL, NULL) != CL_SUCCESS){
 		printf("Error execute\n");
 		free(input);
 		free(output);
 		return 1;
 	}
-	
+	for(size_t i = 1; i < n_iterations; ++i){	//move output to input and start next iteration
+		if(clEnqueueNDRangeKernel(command_queue, kernel_move, 1, NULL,
+  			(const size_t *)&global, NULL, 0, NULL, NULL) != CL_SUCCESS){
+			printf("Error execute\n");
+			free(input);
+			free(output);
+			return 1;
+		}
+		if(clEnqueueNDRangeKernel(command_queue, kernel_heat, 1, NULL,
+  			(const size_t *)&global, NULL, 0, NULL, NULL) != CL_SUCCESS){
+			printf("Error execute\n");
+			free(input);
+			free(output);
+			return 1;
+		}
+	}
+	if(clEnqueueNDRangeKernel(command_queue, kernel_partial, 1, NULL,
+		(const size_t *)&global, NULL, 0, NULL,NULL) != CL_SUCCESS){
+			printf("Error execute partial\n");
+			free(input);
+			free(output);
+			return 1;
+	}
+	//read partial sums
+	double *partial_sums = (double*) malloc(sizeof(double)*n_work_groups);
+	if(clEnqueueReadBuffer(command_queue, partial_sums_buffer, CL_TRUE,
+		0, sizeof(double)*n_work_groups,partial_sums, 0, NULL, NULL) != CL_SUCCESS){
+		printf("Error reading partial sums buffer\n");
+		free(input);
+		free(output);
+		free(partial_sums);
+		return 1;
+	}
+	double average = 0;
+	for(size_t ix = 0; ix < n_work_groups; ++ix){
+		average += partial_sums[ix];
+		printf("%f \n", partial_sums[ix]);
+	}
+	free(partial_sums);
+	average /= global;
+	printf("Average temperature: %f\n", average);
+		
+
 	//read buffer
 	if(clEnqueueReadBuffer(command_queue, output_buffer, CL_TRUE,
-  		0, ix_m*sizeof(long), output, 0, NULL, NULL) != CL_SUCCESS){
+  		0, ix_m*sizeof(double), output, 0, NULL, NULL) != CL_SUCCESS){
 		printf("Error reading buffer\n");
 		free(input);
 		free(output);
@@ -249,7 +355,7 @@ int main(int argc, char *argv[]){
 	//print result here
 	for(int i = 0; i < height; ++i){
 		for(int j = 0; j < width; ++j){
-			printf("%ld ", output[height*i+j]);
+			printf("%.2f ", output[height*i+j]);
 		}
 		printf("\n");
 	}
@@ -266,8 +372,12 @@ int main(int argc, char *argv[]){
 		printf("Error releasing context\n");
 		return 1;
 	}
+	if(clReleaseMemObject(input_buffer) != CL_SUCCESS){
+		printf("Error releasing input buffer\n");
+	}
+	if(clReleaseMemObject(output_buffer) != CL_SUCCESS){
+		printf("Error releasing output buffer\n");
+	}
 	
-	clReleaseMemObject(input_buffer);
-	clReleaseMemObject(output_buffer);
 	return 0;
 }
